a9eb54fc90465628572a57b81b96cb38cac3a60e
umar farooq <umar.farooq@deutics.com>
2022-10-31 18:38:51 +0500

    set pegination and page ui better

--- src/components/blogComponents/BlogComp.js
+++ src/components/blogComponents/BlogComp.js
@@ -1,20 +1,28 @@
+import styled from "styled-components";
+
 const BlogComp = ({ blog }) => {
-	console.log(blog)
+    const Wrapper = styled.div`
+        .title {
+            font-size: 1.6rem;
+        }
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+        }
+        .detail {
+            font-size: 1rem;
+            line-height: 1.2rem;
+        }
+    `;
 
-	return (
-		<div
-			style={{
-				backgroundColor: 'white',
-				alignItems: 'center',
-				textAlign: 'center',
-				padding: '20px',
-			}}
-		>
-			<h1>{blog?.title}</h1>
-			<img src={blog?.mainImage} alt='Blog' />
-			<p>{blog?.body}</p>
-		</div>
-	)
-}
+    return (
+        <Wrapper>
+            <h2 className="title">{blog?.title}</h2>
+            <img src={blog?.mainImage} alt="Blog" />
+            <p>{blog?.body}</p>
+        </Wrapper>
+    );
+};
 
-export default BlogComp
+export default BlogComp;
